49f936d15b848af19f35bb2296c070357bb1d098
落落 <whalefall9420@outlook.com>
2023-11-12 18:25:46 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+import subprocess
+
+setup(
+    name='your-package',
+    version='1.0',
+    packages=['your_package'],
+    install_requires=[],
+    scripts=['pyromod/script.py'],
+)
